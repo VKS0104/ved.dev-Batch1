17440f06c86c738254c88c580f6364afd1b75b70
VKS0104 <vedsingh0104@gmail.com>
2026-01-30 10:59:45 +0530

    first-commit-30Jan

---
+++ script.js
@@ -0,0 +1,19 @@
+const input = document.getElementsByClassName("Search-Bar")[0];
+const btn = document.getElementsByClassName("btn")[0];
+
+btn.addEventListener("click", function () {
+    const location = input.value;
+    console.log(location);
+
+    input.value = "";
+    //call the API
+    fetch("https://api.weatherapi.com/v1/current.json?key=24f082d06eab418895c42819262301&q=pune&aqi=no")
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => {
+            console.error(error);
+        });
+
+});
